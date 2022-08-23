017b7c81bfaa22b03847351af0134020b22e0a43
Ujjwal Aggarwal <f20212427@pilani.bits-pilani.ac.in>
2022-08-23 22:38:49 +0530

    Abstract User Model

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib.auth.admin import UserAdmin
+from .models import NewUser
+
+
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (
+        *UserAdmin.fieldsets,
+        (
+            'Additional Info',
+            {
+                'fields':(
+                    'age','nickname'
+                )
+            }
+        )
+    )
+admin.site.register(NewUser, CustomUserAdmin)
--- users/models.py
+++ users/models.py
@@ -1,36 +1,48 @@
 from django.db import models
-from django.contrib.auth.admin import User
+from django.contrib.auth.models import User, AbstractUser
 # Create your models here.
 
 
-class PersonManagerInactive(models.Manager):
+# class PersonManagerInactive(models.Manager):
 
-    def get_queryset(self):
-        return super().get_queryset().filter(is_active=False)
-
-
-class PersonManagerActive(models.Manager):
-
-    def get_queryset(self):
-        return super(PersonManagerActive, self).get_queryset().filter(is_active=True)
+#     def get_queryset(self):
+#         return super().get_queryset().filter(is_active=False)
 
 
+# class PersonManagerActive(models.Manager):
 
-class Person(User):
+#     def get_queryset(self):
+#         return super(PersonManagerActive, self).get_queryset().filter(is_active=True)
 
-    inactive = PersonManagerInactive()
-    active = PersonManagerActive()
 
-    class Meta:
-        proxy = True
-        ordering = ['first_name']
+# TypeError: Person cannot proxy the swapped model 'user.NewUser'.
+# class Person(User):
 
-    @classmethod
-    def count_all(cls,):
-        return cls.objects.all().count()
+#     inactive = PersonManagerInactive()
+#     active = PersonManagerActive()
+
+#     class Meta:
+#         proxy = True
+#         ordering = ['first_name']
+
+#     @classmethod
+#     def count_all(cls,):
+#         return cls.objects.all().count()
     
-    def check_active(self):
-        return "You are Active" if self.is_active else "You are not active"
+#     def check_active(self):
+#         return "You are Active" if self.is_active else "You are not active"
 
-    def __str__(self) -> str:
-        return self.first_name
+#     def __str__(self) -> str:
+#         return self.first_name
+
+
+class NewUser(AbstractUser):
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    user_id = models.BigAutoField(primary_key=True)
+    age = models.IntegerField(null=True,blank=True)
+    nickname = models.CharField(null=True,blank=True, max_length=50)
+    username = models.CharField(max_length=250)
+    email = models.EmailField(unique=True)
